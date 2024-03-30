8e11b1d0cace2463159b1cb41a913d3b357abb72
ramya72002 <nsriramya7@gmail.com>
2024-03-30 19:38:26 +0530

    image added

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,10 @@
 import FeBg from '@/public/images/about11.jpg'
-import home2 from '@/public/images/home2.jpeg'
+import home2 from '@/public/images/home2.jpg'
 import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section className="relative" style={{backgroundColor:"#04487f"}}>
+    <section className="relative" >
       <div style={{ position: 'relative' }}>
     <Image className="md:max-w-none mx-auto rounded w-full" src={FeBg} alt="Febg" style={{ height: '70vh' }} />
         <div style={{ position: 'absolute', top: '50%', left: '60%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white' }}>
@@ -22,7 +22,7 @@ export default function Hero() {
     <p className="text-lg mb-6">The current literature highlights the prevalence of various cutaneous malignancies in the United States, with melanoma emerging as one of the most frequently diagnosed cancers among young adults. According to findings from The International Agency for Research on Cancer Working Group, tanning beds emit 2-10x more UVA radiation than natural sunlight on average. Additionally, individuals who utilize indoor tanning facilities before the age of 35 years have a 75% increase in the risk of developing melanoma. These statistics emphasize the importance of our commitment to promoting skin cancer prevention measures and cultivating a culture of wellness within our community.</p>
   </div>
   <div className="w-full md:w-1/2">
-    <Image src={home2} alt="Febg" style={{ height: '70vh' }} />
+    <Image src={home2} alt="Febg" style={{ height: '100vh', width:"100%" }} />
   </div>
 </div>
 
