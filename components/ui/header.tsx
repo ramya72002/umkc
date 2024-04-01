7e74a18577a767b51ae5d37dc66e686fe60e0574
ramya72002 <nsriramya7@gmail.com>
2024-04-01 11:37:05 +0530

    changes made for deployment

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,10 +1,6 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
-import Logo from './logo'
-import Dropdown from '@/components/utils/dropdown'
-import MobileMenu from './mobile-menu'
 
 export default function Header() {
   const [top, setTop] = useState<boolean>(true)
